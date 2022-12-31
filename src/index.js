9f622493a140c665e94863023112478642847372
Generosa Litton <glcodeworks@gmail.com>
2022-12-30 20:07:31 -0500

    prop drilling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import "./index.css"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
 const books = [
   {
@@ -15,30 +15,35 @@ const books = [
     img: "./images/it-starts-with-us.jpg",
     id: 2,
   },
-]
+];
 
 const BookList = () => {
+  const getBook = (id) => {
+    const book = books.find((book) => book.id === id);
+    console.log(book);
+  };
   return (
     <section className="booklist">
       {books.map((book) => {
-        return <Book {...book} key={book.id} />
+        return <Book {...book} key={book.id} getBook={getBook} />;
       })}
     </section>
-  )
-}
+  );
+};
 
 const Book = (props) => {
-  const { img, title, author } = props
-  console.log(props)
+  const { img, title, author, id, getBook } = props;
+  // console.log(props);
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
+      <button onClick={() => getBook(id)}>Get This Book</button>
       <h4>{author}</h4>
     </article>
-  )
-}
+  );
+};
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<BookList />)
+root.render(<BookList />);
