1c77c3513f73d59a089fc41d197016441555fe8e
Generosa Litton <glcodeworks@gmail.com>
2023-01-02 14:59:37 -0500

    local images

---
+++ src/books.js
@@ -0,0 +1,24 @@
+import img1 from "./images/atomic-habits.jpeg";
+import img2 from "./images/it-starts-with-us.jpg";
+import img3 from "./images/spare.jpg";
+
+export const books = [
+  {
+    author: "James Clear",
+    title: "Atomic Habits",
+    img: img1,
+    id: 1,
+  },
+  {
+    author: "Colleen Hoover",
+    title: "It Starts With Us",
+    img: img2,
+    id: 2,
+  },
+  {
+    author: "Prince Harry The Duke of Sussex",
+    title: "Spare",
+    img: img3,
+    id: 3,
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,46 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 
-const books = [
-  {
-    author: "James Clear",
-    title: "Atomic Habits",
-    img: "./images/atomic-habits.jpeg",
-    id: 1,
-  },
-  {
-    author: "Colleen Hoover",
-    title: "It Starts With Us",
-    img: "./images/it-starts-with-us.jpg",
-    id: 2,
-  },
-];
+import "./index.css";
+import { books } from "./books";
+import Book from "./Book";
 
 const BookList = () => {
-  const getBook = (id) => {
-    const book = books.find((book) => book.id === id);
-    console.log(book);
-  };
   return (
     <section className="booklist">
       {books.map((book) => {
-        return <Book {...book} key={book.id} getBook={getBook} />;
+        return <Book {...book} key={book.id} />;
       })}
     </section>
-  );
-};
-
-const Book = (props) => {
-  const { img, title, author, id, getBook } = props;
-  // console.log(props);
-  return (
-    <article className="book">
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <button onClick={() => getBook(id)}>Get This Book</button>
-      <h4>{author}</h4>
-    </article>
   );
 };
 
