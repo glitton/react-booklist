215f39274b6ba70d930614e6e3f9b2c320461ee3
Generosa Litton <glcodeworks@gmail.com>
2023-01-02 15:16:45 -0500

    book number and title

--- src/Book.js
+++ src/Book.js
@@ -1,8 +1,9 @@
 export const Book = (props) => {
-  const { img, title, author } = props;
+  const { img, title, author, number } = props;
 
   return (
     <article className="book">
+      <span className="number">{`# ${number + 1}`}</span>
       <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author}</h4>
